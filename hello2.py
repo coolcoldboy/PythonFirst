a6cd9d4180cee1b8b5d5e3804763defdbca26a71
coolcoldboy <cool.coldboy@gmail.com>
2016-09-11 14:03:29 +0800

    Add files via upload

---
+++ hello2.py
@@ -0,0 +1,6 @@
+import math
+def HelloWorld():
+	x = (input("input a integer:")) 
+	print ("\n"+math.sqrt(float(x)))
+if __name__=="__main__":
+    HelloWorld()
---
+++ hello3.py
@@ -0,0 +1,7 @@
+def HelloWorld():
+	print ("Hello World!")
+
+if __name__=="__main__":
+    HelloWorld()
+    print('abc')
+    f=open(r'c:/test.txt')
